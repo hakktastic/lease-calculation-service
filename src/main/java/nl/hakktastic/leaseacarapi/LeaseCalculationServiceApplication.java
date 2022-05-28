38078f93493acc473dd5f5b10497ae5991dc3e10
hakktastic <hakktastic@gmail.com>
2022-05-28 15:30:12 +0200

    enable openfeign
    in order to easly connect to rest services needed

--- src/main/java/nl/hakktastic/leaseacarapi/LeaseCalculationServiceApplication.java
+++ src/main/java/nl/hakktastic/leaseacarapi/LeaseCalculationServiceApplication.java
@@ -2,8 +2,10 @@ package nl.hakktastic.leaseacarapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("nl.hakktastic.leaseacarapi")
 public class LeaseCalculationServiceApplication {
 
     public static void main(String[] args) {
