508cc4a4ef2365b1639734674d119f7a4dd95a18
hakktastic <hakktastic@gmail.com>
2022-05-28 14:49:11 +0200

    initial commit

---
+++ src/main/java/nl/hakktastic/leaseacarapi/LeaseCalculationServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.hakktastic.leaseacarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaseCalculationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeaseCalculationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/hakktastic/leaseacarapi/LeaseCalculationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hakktastic.leaseacarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaseCalculationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
