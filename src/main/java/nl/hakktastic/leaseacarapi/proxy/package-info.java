36961ae2f887982327e867475d959765ce5117bf
hakktastic <hakktastic@gmail.com>
2022-07-18 18:23:38 +0200

    HAK-49 add package-info.java
    for package-level documentation and annotations

---
+++ src/main/java/nl/hakktastic/leaseacarapi/proxy/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package is about the {@link org.springframework.cloud.openfeign.FeignClient} objects of the REST service.
+ *
+ */
+@NonNullApi
+@NonNullFields
+package nl.hakktastic.leaseacarapi.proxy;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
---
+++ src/main/java/nl/hakktastic/leaseacarapi/service/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package is about the {@link org.springframework.stereotype.Service} objects of the REST service.
+ *
+ */
+@NonNullApi
+@NonNullFields
+package nl.hakktastic.leaseacarapi.service;
+
+import org.springframework.lang.NonNullApi;
+import org.springframework.lang.NonNullFields;
