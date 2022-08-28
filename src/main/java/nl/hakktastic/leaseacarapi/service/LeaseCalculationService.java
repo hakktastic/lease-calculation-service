d8f72f49e6e6dac9c48433e23606ce7182763539
hakktastic <hakktastic@gmail.com>
2022-08-28 16:12:43 +0200

    HAK-42 fix logging
    because static logger was used instead of Slf4j

--- src/main/java/nl/hakktastic/leaseacarapi/service/LeaseCalculationService.java
+++ src/main/java/nl/hakktastic/leaseacarapi/service/LeaseCalculationService.java
@@ -5,7 +5,6 @@ import nl.hakktastic.leaseacarapi.proxy.CarServiceProxy;
 import nl.hakktastic.leaseacarapi.proxy.CustomerServiceProxy;
 import nl.hakktastic.leaseacarapi.proxy.InterestRateServiceProxy;
 import org.apache.commons.lang3.Validate;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +16,6 @@ import java.math.RoundingMode;
 @Slf4j
 public class LeaseCalculationService {
 
-  private final Logger logger = org.slf4j.LoggerFactory.getLogger(this.getClass());
-
   @Autowired private CarServiceProxy carServiceProxy;
 
   @Autowired private InterestRateServiceProxy interestRateProxy;
@@ -29,7 +26,8 @@ public class LeaseCalculationService {
       int carId, int mileage, int duration, int interestRateId, int customerId) {
 
     var carBean = this.carServiceProxy.getCarById(carId).orElseGet(() -> null);
-    var interestRateBean = this.interestRateProxy.getInterestById(interestRateId).orElseGet(() -> null);
+    var interestRateBean =
+        this.interestRateProxy.getInterestById(interestRateId).orElseGet(() -> null);
     var customerBean = this.customerServiceProxy.getCustomerById(customerId).orElseGet(() -> null);
 
     Validate.notNull(carBean, "unable to retreive car entity with ID: {}", carId);
@@ -37,17 +35,17 @@ public class LeaseCalculationService {
         interestRateBean, "unable to retreive interest rate entity with ID: {}", interestRateId);
     Validate.notNull(customerBean, "unable to retrieve customer entity with ID: {}", customerId);
 
-      this.logger.info("Mileage --> {}", mileage);
-      this.logger.info("Duration --> {}", duration);
-      this.logger.info("Car entity --> {}", carBean);
-      this.logger.info("Interest Rate entity --> {}", interestRateBean);
-      this.logger.info("Customer entity --> {}", customerBean);
+    log.info("Mileage --> {}", mileage);
+    log.info("Duration --> {}", duration);
+    log.info("Car entity --> {}", carBean);
+    log.info("Interest Rate entity --> {}", interestRateBean);
+    log.info("Customer entity --> {}", customerBean);
 
     var leaseRate =
         (((mileage / 12) * duration) / carBean.getNettPrice())
             + (((interestRateBean.getInterestRate() / 100) * carBean.getNettPrice()) / 12);
 
-      this.logger.info("Lease Rate --> {}", leaseRate);
+    log.info("Lease Rate --> {}", leaseRate);
 
     return new BigDecimal(leaseRate).setScale(2, RoundingMode.HALF_DOWN).doubleValue();
   }
