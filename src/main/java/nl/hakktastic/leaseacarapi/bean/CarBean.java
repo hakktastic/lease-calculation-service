21221ab85190ad7f14a74634bbce3ee310066189
hakktastic <hakktastic@gmail.com>
2022-08-28 16:04:00 +0200

    HAK-42 replace boiler code with Lombok annotations
    -

--- src/main/java/nl/hakktastic/leaseacarapi/bean/CarBean.java
+++ src/main/java/nl/hakktastic/leaseacarapi/bean/CarBean.java
@@ -1,10 +1,15 @@
 package nl.hakktastic.leaseacarapi.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.apache.commons.lang3.builder.EqualsBuilder;
+import lombok.NoArgsConstructor;
 
 /** Car Bean. */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class CarBean {
 
   private int id;
@@ -15,58 +20,4 @@ public class CarBean {
   private double grossPrice;
   private double nettPrice;
   private int hp;
-
-  /** Default Constructor. */
-  public CarBean() {}
-
-  /**
-   * Constructor with all fields.
-   *
-   * @param id ID of the car object
-   * @param make make of the car object
-   * @param model model of the car object
-   * @param version version of the car object
-   * @param numberOfDoors # doors of the car object
-   * @param grossPrice gross price of the car object
-   * @param nettPrice nett price of the car object
-   * @param hp horse power of the car object
-   */
-  public CarBean(
-      int id,
-      String make,
-      String model,
-      String version,
-      int numberOfDoors,
-      double grossPrice,
-      double nettPrice,
-      int hp) {
-    super();
-    this.id = id;
-    this.make = make;
-    this.model = model;
-    this.version = version;
-    this.numberOfDoors = numberOfDoors;
-    this.grossPrice = grossPrice;
-    this.nettPrice = nettPrice;
-    this.hp = hp;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-
-    boolean equation = false;
-
-    if (obj instanceof CarBean) {
-
-      final CarBean otherEntity = (CarBean) obj;
-
-      equation =
-          new EqualsBuilder()
-              .appendSuper(super.equals(obj))
-              .append(this.getId(), otherEntity.getId())
-              .isEquals();
-    }
-
-    return equation;
-  }
 }
--- src/main/java/nl/hakktastic/leaseacarapi/bean/InterestRateBean.java
+++ src/main/java/nl/hakktastic/leaseacarapi/bean/InterestRateBean.java
@@ -1,51 +1,20 @@
 package nl.hakktastic.leaseacarapi.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import org.apache.commons.lang3.builder.EqualsBuilder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 /** Interest Rate Bean. */
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class InterestRateBean {
 
   private int id;
   private double interestRate;
   private LocalDate startDate;
-
-  /** Default constructor. */
-  public InterestRateBean() {}
-
-  /**
-   * Constructor with all fields.
-   *
-   * @param id ID of interest rate object
-   * @param interestRate interest rate
-   * @param startDate start date for the interest rate
-   */
-  public InterestRateBean(int id, double interestRate, LocalDate startDate) {
-
-    this.id = id;
-    this.interestRate = interestRate;
-    this.startDate = startDate;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-
-    boolean equation = false;
-
-    if (obj instanceof InterestRateBean) {
-
-      final InterestRateBean otherEntity = (InterestRateBean) obj;
-
-      equation =
-          new EqualsBuilder()
-              .appendSuper(super.equals(obj))
-              .append(this.getId(), otherEntity.getId())
-              .isEquals();
-    }
-
-    return equation;
-  }
 }
