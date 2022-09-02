cf12b8b86855c6583f1e29691149b425c8b9ce38
hakktastic <hakktastic@gmail.com>
2022-09-02 17:06:16 +0200

    HAK-42 simplify implementation
    by delegating the calculation and state of arguments to the LeaseRateBean object

--- src/main/java/nl/hakktastic/leaseacarapi/service/LeaseCalculationService.java
+++ src/main/java/nl/hakktastic/leaseacarapi/service/LeaseCalculationService.java
@@ -1,15 +1,14 @@
 package nl.hakktastic.leaseacarapi.service;
 
 import lombok.extern.slf4j.Slf4j;
+import nl.hakktastic.leaseacarapi.bean.LeaseRateBean;
 import nl.hakktastic.leaseacarapi.proxy.CarServiceProxy;
 import nl.hakktastic.leaseacarapi.proxy.CustomerServiceProxy;
 import nl.hakktastic.leaseacarapi.proxy.InterestRateServiceProxy;
-import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
+import java.util.OptionalDouble;
 
 /** Service class for managing the calculation of the lease rate. */
 @Service
@@ -22,31 +21,18 @@ public class LeaseCalculationService {
 
   @Autowired private CustomerServiceProxy customerServiceProxy;
 
-  public double calculateLeaseRate(
+  public OptionalDouble calculateLeaseRate(
       int carId, int mileage, int duration, int interestRateId, int customerId) {
 
-    var carBean = this.carServiceProxy.getCarById(carId).orElseGet(() -> null);
-    var interestRateBean =
-        this.interestRateProxy.getInterestById(interestRateId).orElseGet(() -> null);
-    var customerBean = this.customerServiceProxy.getCustomerById(customerId).orElseGet(() -> null);
+    var leaseRateBean =
+        LeaseRateBean.builder()
+            .car(this.carServiceProxy.getCarById(carId).orElse(null))
+            .customer(this.customerServiceProxy.getCustomerById(customerId).orElse(null))
+            .interestRate(this.interestRateProxy.getInterestById(interestRateId).orElse(null))
+            .build();
 
-    Validate.notNull(carBean, "unable to retreive car entity with ID: {}", carId);
-    Validate.notNull(
-        interestRateBean, "unable to retreive interest rate entity with ID: {}", interestRateId);
-    Validate.notNull(customerBean, "unable to retrieve customer entity with ID: {}", customerId);
+    log.info("Lease Rate --> {}", leaseRateBean.toString());
 
-    log.info("Mileage --> {}", mileage);
-    log.info("Duration --> {}", duration);
-    log.info("Car entity --> {}", carBean);
-    log.info("Interest Rate entity --> {}", interestRateBean);
-    log.info("Customer entity --> {}", customerBean);
-
-    var leaseRate =
-        (((mileage / 12) * duration) / carBean.getNettPrice())
-            + (((interestRateBean.getInterestRate() / 100) * carBean.getNettPrice()) / 12);
-
-    log.info("Lease Rate --> {}", leaseRate);
-
-    return new BigDecimal(leaseRate).setScale(2, RoundingMode.HALF_DOWN).doubleValue();
+    return leaseRateBean.getLeaseRate();
   }
 }
